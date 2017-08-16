98cc242887fb250578d8841db56377f8b75c01b1
bradberger01 <bradberger@hotmail.com>
2017-08-16 18:37:40 -0400

    Word Problems

--- 081517_VariablesOperatorsExpressions/Program.cs
+++ 081517_VariablesOperatorsExpressions/Program.cs
@@ -36,21 +36,51 @@ namespace _081517_VariablesOperatorsExpressions
             //Console.WriteLine(partialNumber);
 
             //boolean
-            bool isPresent = true;
-            bool seatTaken = false;
+            //bool isPresent = true;
+            //bool seatTaken = false;
 
-            //Character
-            char lastLetter = 'j';
+            ////Character
+            //char lastLetter = 'j';
 
-            int jessicaAge = 23;
-            int samAge = 47;
-            Console.WriteLine(jessicaAge > samAge);
+            //int jessicaAge = 23;
+            //int samAge = 47;
+            //Console.WriteLine(jessicaAge > samAge);
 
             int mikeBill = 13;
             int jimBill = 13;
             int frankBill = 13;
             int benBill = 13;
             Console.WriteLine(mikeBill + jimBill + frankBill + benBill);
+
+            int totalCups = 7;
+            int usedCups = 2;
+            Console.WriteLine(totalCups - usedCups);
+
+            int oneDiaper = 8;
+            int moneyTotal = 40;
+            Console.WriteLine(moneyTotal / oneDiaper);
+
+            int treTotal = 41;
+            int treFri = 29;
+            Console.WriteLine(treTotal - treFri);
+
+            int pranRun = 47;
+            Console.WriteLine(pranRun + 30);
+
+            int boxMoney = 12;
+            int perBox = 3;
+            Console.WriteLine(boxMoney / perBox);
+
+            float newTotal = 27.10f;
+            float saladCost = 5.12f;
+            Console.WriteLine(newTotal + saladCost);
+
+            int totalStudents = 331;
+            int carStudents = 7;
+            int busStudents = (totalStudents - carStudents);
+            Console.WriteLine(busStudents / 6);
+
+
 
 
 
